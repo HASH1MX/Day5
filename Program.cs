f8c709c54efda379b2b8cb43779dfd1df7004b5b
HASH1MX <hashim8work@gmail.com>
2023-05-29 18:03:33 +0500

    Day5 Array Program

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           //string folderPath = "D:\\Coding\\KB\\Cheat Sheet";
+           
+           //string folderPath = Console.ReadLine();
+            string folderPath = args[0];
+
+             DirectoryInfo dir = new DirectoryInfo(folderPath);
+
+             FileInfo[] files = dir.GetFiles("*.*");
+
+             foreach(FileInfo f in files )
+               {
+
+                 Console.WriteLine(f);
+                
+                }
+
+            //Console.WriteLine(dir);
+            //Console.WriteLine(folderPath); 
+
+        }
+    }
+}
